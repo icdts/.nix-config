c2e627954232bee711f1150bb16e8d2294b13603
icdts <icandothings@gmail.com>
2025-03-21 21:03:40 -0500

    catppuccin update, kernel 6_12

--- system/nixos.nix
+++ system/nixos.nix
@@ -19,7 +19,7 @@
             {
               boot.loader.systemd-boot.enable = true;
               boot.loader.efi.canTouchEfiVariables = true;
-              boot.kernelPackages = pkgs.linuxPackages_latest;
+              boot.kernelPackages = pkgs.linuxPackages_6_12;
               networking.networkmanager.enable = true;
 
               time.timeZone = "America/Chicago";
