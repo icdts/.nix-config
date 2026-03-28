6d1c486332ed393951478b38fa2582ff98069784
icdts <icandothings@gmail.com>
2026-03-28 12:05:27 -0500

    gtk4.theme = null to remove deprecation warning, updates

--- module/home-manager/desktop/default.nix
+++ module/home-manager/desktop/default.nix
@@ -44,6 +44,7 @@ in
       enable = true;
       gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
       gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
+      gtk4.theme = null;
     };
 
     programs = {
